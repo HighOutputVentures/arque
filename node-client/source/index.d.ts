d36e6357b2e86c5476ddf886364b94188970726d
ccpacillos <clint.pacillos@gmail.com>
2021-11-11 20:22:32 +0800

    feat(node-client): setup project files

--- node-client/source/index.d.ts
+++ node-client/source/index.d.ts
@@ -6,7 +6,7 @@ type Event<TType extends string = string, TData = unknown, TMeta = unknown> = {
   data: TData;
   meta: TMeta;
   timestamp: Date;
-}
+};
 
 interface Aggregate<TState> {
   id: Buffer;
@@ -24,22 +24,32 @@ interface Projection<TState> {
 
 interface Connection {
   loadAggregate<TState>(
-    id: Buffer;
+    id: Buffer,
     options?: {
-      eventHandlers?: Record<string, (event: Event, state: TState) => Promise<TState> | TState>;
+      eventHandlers?: Record<
+        string,
+        (event: Event, state: TState) => Promise<TState> | TState
+      >;
       onInitializeState?: () => Promise<TState> | TState;
       onShouldTakeSnapshot?: (event: Event, state: TState) => Promise<boolean>;
       onLoadSnapshot?: (aggregateId: Buffer) => Promise<{
         aggregateVersion: number;
         state: TState;
       }>;
-      onTakeSnapshot?: (aggregateId: Buffer, aggregateVersion: number, state: TState) => Promise<void>;
+      onTakeSnapshot?: (
+        aggregateId: Buffer,
+        aggregateVersion: number,
+        state: TState,
+      ) => Promise<void>;
     },
-  ): Promise<Aggregate<TState>>,
+  ): Promise<Aggregate<TState>>;
   initializeProjection<TState>(
-    id: Buffer;
+    id: Buffer,
     options?: {
-      eventHandlers?: Record<string, (event: Event, state: TState) => Promise<TState> | TState>;
+      eventHandlers?: Record<
+        string,
+        (event: Event, state: TState) => Promise<TState> | TState
+      >;
       onInitializeState?: () => Promise<TState> | TState;
       autoStart?: true;
     },
@@ -54,16 +64,18 @@ interface Arque {
       {
         hostname?: string;
         port?: number;
-      }
-    ]
-  }): Promise<Connection>,
+      },
+    ];
+  }): Promise<Connection>;
 }
 
 interface Page<TNode> {
-  edges: [{
-    node: TNode;
-    cursor: Buffer;
-  }];
+  edges: [
+    {
+      node: TNode;
+      cursor: Buffer;
+    },
+  ];
   pageInfo: {
     hasNextPage: boolean;
     endCursor: null | Buffer;
@@ -78,8 +90,8 @@ interface Driver {
       {
         hostname?: string;
         port?: number;
-      }
-    ]
+      },
+    ];
   }): Promise<void>;
   createEvent(params: {
     type: string;
@@ -88,11 +100,19 @@ interface Driver {
     data: Record<string, any>;
     meta?: Record<string, any>;
   }): Promise<void>;
-  readAggregateEvents(first?: number, after?: Buffer, params: {
-    aggregateId: Buffer;
-  }): Promise<Page<Event>>;
-  readEvents(first?: number, after?: Buffer, params: {
-    types: string[]
-  }): Promise<Page<Event>>;
+  readAggregateEvents(
+    first?: number,
+    after?: Buffer,
+    params: {
+      aggregateId: Buffer;
+    },
+  ): Promise<Page<Event>>;
+  readEvents(
+    first?: number,
+    after?: Buffer,
+    params: {
+      types: string[];
+    },
+  ): Promise<Page<Event>>;
   subscribe(): Promise<Stream>;
 }
